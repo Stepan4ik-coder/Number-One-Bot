0d621ac675f676a8af65fab37da57218bd5fbec9
Stepan4ik-coder <yctepan@list.ru>
2026-01-05 14:09:25 +1000

    done

--- handlers/default_handlers/echo.py
+++ handlers/default_handlers/echo.py
@@ -2,9 +2,8 @@ from telebot.types import Message
 from loader import bot
 
 
-# Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @bot.message_handler(state=None)
 def bot_echo(message: Message):
     bot.reply_to(
-        message, "Эхо без состояния или фильтра.\n" f"Сообщение: {message.text}"
+        message, f"🤔 Не понимаю сообщение '{message.text}'... Попробуй /help 📚"
     )
